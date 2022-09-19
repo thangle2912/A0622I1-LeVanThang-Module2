7ce4c8f86051423bcc6736acce9dedc153380bc1
Thang Le <thangle2912@gmail.com>
2022-09-19 18:28:53 +0700

    baitap

--- src/ss6_inheritance/bai_tap/point_moveable_point/MovablePoint.java
+++ src/ss6_inheritance/bai_tap/point_moveable_point/MovablePoint.java
@@ -1,4 +1,59 @@
 package src.ss6_inheritance.bai_tap.point_moveable_point;
 
-public class MoveablePoint {
+public class MovablePoint extends Point{
+    private float xSpeed;
+    private float ySpeed;
+
+    public MovablePoint() {
+    }
+
+    public MovablePoint(float xSpeed, float ySpeed) {
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+
+    public MovablePoint(float x, float y, float xSpeed, float ySpeed) {
+        super(x, y);
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+
+    public float getXSpeed() {
+        return xSpeed;
+    }
+
+    public void setXSpeed(float xSpeed) {
+        this.xSpeed = xSpeed;
+    }
+
+    public float getYSpeed() {
+        return ySpeed;
+    }
+
+    public void setYSpeed(float ySpeed) {
+        this.ySpeed = ySpeed;
+    }
+    public void setSpeed(float xSpeed,float ySpeed) {
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+    public float[] getSpeed() {
+        float array[] = new float[2];
+        array[0] = this.xSpeed;
+        array[1] = this.ySpeed;
+        return array;
+    }
+    public void move(){
+        this.setX(getX()+this.xSpeed);
+        this.setY(getY()+this.ySpeed);
+    }
+    @Override
+    public String toString() {
+        return "MovablePoint{" +
+                "x="+getX()+
+                ", y="+getY()+
+                ", xSpeed=" + xSpeed +
+                ", ySpeed=" + ySpeed +
+                '}';
+    }
 }
--- src/ss6_inheritance/bai_tap/point_moveable_point/Test.java
+++ src/ss6_inheritance/bai_tap/point_moveable_point/Test.java
@@ -1,4 +1,16 @@
 package src.ss6_inheritance.bai_tap.point_moveable_point;
 
 public class Test {
+    public static void main(String[] args) {
+        Point point = new Point();
+        point.setXY(2,3);
+        System.out.println(point.toString());
+
+        MovablePoint movablePoint = new MovablePoint();
+        movablePoint.setXY(5,6);
+        movablePoint.setSpeed(5,5);
+        System.out.println(movablePoint.toString());
+        movablePoint.move();
+        System.out.println(movablePoint.toString());
+    }
 }
