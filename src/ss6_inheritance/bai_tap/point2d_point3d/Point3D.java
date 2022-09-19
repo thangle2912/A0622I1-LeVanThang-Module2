1e9e111a9eb5dcef1587060b81106ff471928df8
Thang Le <thangle2912@gmail.com>
2022-09-19 18:29:36 +0700

    baitap

--- src/ss6_inheritance/bai_tap/point2d_point3d/Point3D.java
+++ src/ss6_inheritance/bai_tap/point2d_point3d/Point3D.java
@@ -1,4 +1,36 @@
 package src.ss6_inheritance.bai_tap.point2d_point3d;
 
-public class Point3D {
+public class Point3D extends Point2D{
+    private float z;
+
+    public Point3D() {
+    }
+
+    public Point3D(float x, float y, float z) {
+        super(x, y);
+        this.z = z;
+    }
+
+    public float getZ() {
+        return z;
+    }
+
+    public void setZ(float z) {
+        this.z = z;
+    }
+    public void setXYZ(float x, float y,float z){
+        setXY(x,y);
+        this.z =z;
+
+    }
+    public float[] getXYZ(){
+        float arr[] = new float[3];
+        arr[0] = getX();
+        arr[1] = getY();
+        arr[2] = getZ();
+        return arr;
+    }
+    public String toString(){
+        return "x : "+getX() +"  y : "+getY() +"  z : "+getZ();
+    }
 }
--- src/ss6_inheritance/bai_tap/point2d_point3d/Test.java
+++ src/ss6_inheritance/bai_tap/point2d_point3d/Test.java
@@ -1,4 +1,19 @@
 package src.ss6_inheritance.bai_tap.point2d_point3d;
 
 public class Test {
+    public static void main(String[] args) {
+        Point2D point2D = new Point2D();
+        point2D.setXY(2,3);
+        for (float element : point2D.getXY()){
+            System.out.println(element);
+        }
+        System.out.println( point2D.toString());
+
+        Point3D point3D = new Point3D();
+        point3D.setXYZ(3,4,5);
+        for (float element : point3D.getXYZ()){
+            System.out.println(element);
+        }
+        System.out.println(point3D.toString());
+    }
 }
