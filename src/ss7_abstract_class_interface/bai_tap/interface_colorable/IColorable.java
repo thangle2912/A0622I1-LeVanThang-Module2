0d429310b142bddf3bfb505fdec190fd959eec8d
Thang Le <thangle2912@gmail.com>
2022-09-21 19:35:02 +0700

    baitap-ss7

--- src/ss7_abstract_class_interface/bai_tap/interface_colorable/IColorable.java
+++ src/ss7_abstract_class_interface/bai_tap/interface_colorable/IColorable.java
@@ -1,4 +1,5 @@
 package src.ss7_abstract_class_interface.bai_tap.interface_colorable;
 
 public interface IColorable {
+    void howToColor();
 }
--- src/ss7_abstract_class_interface/bai_tap/interface_colorable/Test.java
+++ src/ss7_abstract_class_interface/bai_tap/interface_colorable/Test.java
@@ -1,4 +1,23 @@
 package src.ss7_abstract_class_interface.bai_tap.interface_colorable;
 
+import src.ss7_abstract_class_interface.bai_tap.interface_resizeable.Circle;
+import src.ss7_abstract_class_interface.bai_tap.interface_resizeable.Rectangle;
+import src.ss7_abstract_class_interface.bai_tap.interface_resizeable.Shape;
+import src.ss7_abstract_class_interface.bai_tap.interface_resizeable.Square;
+
 public class Test {
+    public static void main(String[] args) {
+        Shape[] objects = new Shape[4];
+        objects[0] = new Square(5);
+        objects[1] = new Rectangle(7, 4);
+        objects[2] = new Square(5);
+        objects[3] = new Circle(3);
+
+        for (Shape object : objects) {
+            System.out.println(object.getName());
+            object.howToColor();
+            System.out.println(object.getArea());
+
+        }
+    }
 }
